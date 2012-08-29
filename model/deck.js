aa581ee397ab6330bdc11762ee3bb88c3f32daa2
Jamund Ferguson <jamund@gmail.com>
2012-08-28 22:50:58 -0600

    All Views Work - Including Singel Slide View

--- model/deck.js
+++ model/deck.js
@@ -2,10 +2,10 @@ module.exports = [{
   id: 0,
   title: "Jamund's Vacation",
   author: "Jamund Ferguson",
-  slides: [{text: "Paris"}, {text: "Rome"}, {text: "Malta"}, {text: "Spain"}, {text: "London"}, {text: "Brugges"}, {text: "Rotterdam"}]
+  slides: [{img: "http://farm8.staticflickr.com/7271/7588148580_89b3b0968c_m.jpg", title: "Paris"}, {img: "http://farm8.staticflickr.com/7271/7588148580_89b3b0968c_m.jpg", title: "Rome"}, {img: "http://farm8.staticflickr.com/7271/7588148580_89b3b0968c_m.jpg",title: "Malta"}, {img: "http://farm8.staticflickr.com/7271/7588148580_89b3b0968c_m.jpg",title: "Spain"}, {img: "http://farm8.staticflickr.com/7271/7588148580_89b3b0968c_m.jpg",title: "London"}, {img: "http://farm8.staticflickr.com/7271/7588148580_89b3b0968c_m.jpg",title: "Brugges"}, {img: "http://farm8.staticflickr.com/7271/7588148580_89b3b0968c_m.jpg",title: "Rotterdam"}]
 }, {
   id: 1,
   title: "Mobile Web Deck",
   author: "Jamund Ferguson",
-  slides: [{text:"Mobile Web Deck"}, {text: "Go Mobile!"}, {text: "The End!"}]
+  slides: [{title:"Mobile Web Deck"}, {title: "Go Mobile!"}, {title: "The End!"}]
 }];
--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,16 @@
 var Slide = Backbone.Model.extend({});
 var Deck = Backbone.Model.extend({});
+var $content = $('.content');
+var $title = $('#title');
 var SlideList = Backbone.Collection.extend({
   model: Slide
 });
+
 var DeckList = Backbone.Collection.extend({
   model: Deck,
   url: '/decks'
 });
-var Slides = new SlideList();
-var Decks = new DeckList();
+
 var DeckView = Backbone.View.extend({
   template: Handlebars.compile($("#template-deck").html()),
   render: function() {
@@ -16,23 +18,93 @@ var DeckView = Backbone.View.extend({
     return this;
   }
 });
-var SlideView = Backbone.View.extend({
-  template: Handlebars.compile($("template-slide").html())
-})
 
+var SlidesView = Backbone.View.extend({
+  el: $content,
+  initialize: function() {
+    this.render();
+  },
+  render: function() {
+    var self = this;
+    $title.text(this.model.get('title'));
+    $(this.el).empty();
+    this.model.get("slides").forEach(function(slide, i) {
+      slide.deckId = self.model.get('id');
+      slide.num = i;
+      var view = new SlideView({model:new Slide(slide)});
+      $(self.$el).append(view.render().el);
+    });
+    return this;
+  }
+});
+
+var SlideView = Backbone.View.extend({
+  template: Handlebars.compile($("#template-slide").html()),
+  render: function() {
+    this.el = this.template(this.model.toJSON());
+    return this;
+  }
+});
 var DecksView = Backbone.View.extend({
-  el: $("#decks"),
+  el: $content,
   initialize: function() {
     this.collection.fetch();
     this.collection.on('reset', this.render.bind(this));
   },
   render: function() {
     var self = this;
+    $title.text('All Slides');
+    $(this.el).empty();
     this.collection.forEach(function(deck) {
       var view = new DeckView({model: deck});
       $(self.el).append(view.render().el);
     });
+    return this;
   }
 });
+var Decks = new DeckList();
+var AppRouter = Backbone.Router.extend({
+    initialize: function() {
+      Backbone.history.start(); // {pushState: true});
+    },  
+    routes: {
+      "": "decks",
+      "decks/:deckId": "deck",
+      "slides/:deckId/:num": "slide"
+    },
+    decks: function() {
+      var decks = new DecksView({collection: Decks});
+    },
+    deck: function(deckId) {
+      if (!Decks.length) {
+        Decks.fetch({success: loadSlide});
+      } else {
+        loadSlides();
+      }
+      
+      function loadSlides() {
+          var deck = new SlidesView({model: Decks.get(deckId)});
+      }
+    },
+    slide: function(deckId, num) {
+      if (!Decks.length) {
+        Decks.fetch({success: loadSlide});
+      } else {
+        loadSlide();
+      }
+      
+      function loadSlide() {
+        var deck = Decks.get(deckId);
+        var slide = deck.get('slides')[num];
+        slide.deckId = deckId;
+        slide.num = num;
+        $title.text(slide.title);
+        var s = new Slide(slide);
+        var view = new SlideView({model: s});
+        $content.html(view.render().el);
+      }
+    }
+});
 
-var app = new DecksView({collection: Decks});
+var router = new AppRouter();
+
